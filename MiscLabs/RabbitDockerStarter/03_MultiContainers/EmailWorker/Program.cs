1f187474a0d020950a1c44020a226266727f526b
Changhui Xu <changhuixu@hotmail.com>
2020-05-24 00:37:45 -0500

    RabbitMQ: add a multi-container example

---
+++ MiscLabs/RabbitDockerStarter/03_MultiContainers/EmailWorker/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
+namespace EmailWorker
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var factory = new ConnectionFactory
+            {
+                UserName = "ops1",
+                Password = "ops1"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            const string queueName = "emailworker";
+            channel.QueueDeclarePassive(queueName);
+            Console.WriteLine($"Queue [{queueName}] is waiting for messages.");
+
+            var messageCount = channel.MessageCount(queueName);
+            if (messageCount > 0)
+            {
+                Console.WriteLine($"\tDetected {messageCount} message(s).");
+            }
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (bc, ea) =>
+            {
+                if (ea.BasicProperties.UserId != "ops0")
+                {
+                    Console.WriteLine($"\tIgnored a message sent by [{ea.BasicProperties.UserId}].");
+                    return;
+                }
+
+                try
+                {
+                    var t = DateTimeOffset.FromUnixTimeMilliseconds(ea.BasicProperties.Timestamp.UnixTime);
+                    Console.WriteLine($"{t.LocalDateTime:O} ID=[{ea.BasicProperties.MessageId}]");
+                    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                    var order = JsonSerializer.Deserialize<Order>(message);
+                    Console.WriteLine($"\tProcessing order: '{message}'.");
+                    Thread.Sleep((new Random().Next(1, 3)) * 1000);
+                    Console.WriteLine($"\tOrder #[{order.Id} confirmation sent to [{order.Email}].");
+                    var model = ((IBasicConsumer)bc).Model;
+                    model.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (AlreadyClosedException)
+                {
+                    Console.WriteLine("RabbitMQ is closed!");
+                }
+            };
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+            connection.Close();
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ MiscLabs/RabbitDockerStarter/03_MultiContainers/OrderApi/Controllers/OrdersController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace OrderApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ILogger<OrdersController> _logger;
+        private readonly List<Order> _orders;
+        private readonly ConnectionFactory _connectionFactory;
+
+        public OrdersController(ILogger<OrdersController> logger, ConnectionFactory connectionFactory)
+        {
+            _logger = logger;
+            _connectionFactory = connectionFactory;
+            _orders = new List<Order>();
+        }
+
+        [HttpGet]
+        public IEnumerable<Order> Get()
+        {
+            return _orders;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var order = _orders.SingleOrDefault(x => x.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public IActionResult Post(NewOrderRequest request)
+        {
+            var newOrder = new Order
+            {
+                Id = _orders.Max(x => x.Id) + 1,
+                Email = request.Email
+            };
+            _orders.Add(newOrder);
+            var newOrderJson = JsonSerializer.Serialize(newOrder);
+            _logger.LogInformation($"New order created: {newOrderJson}");
+
+            using var connection = _connectionFactory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var props = channel.CreateBasicProperties();
+            props.AppId = "OrderApi";
+            props.Persistent = true;
+            props.UserId = _connectionFactory.UserName;
+            props.MessageId = Guid.NewGuid().ToString("N");
+            props.Timestamp = new AmqpTimestamp(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+            var body = Encoding.UTF8.GetBytes(newOrderJson);
+            channel.BasicPublish("emails", "order.created", props, body);
+
+            return CreatedAtAction(nameof(GetById), newOrder.Id, newOrder);
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class NewOrderRequest
+    {
+        public string Email { get; set; }
+    }
+}
